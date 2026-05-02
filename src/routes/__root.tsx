c84068ae8586841f594ae4536c775dd80d169faa
lwooden <lowell.wooden@gmail.com>
2026-05-02 13:53:37 -0400

    theming

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -47,7 +47,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className="dark">
+		<html lang="en" className="light">
 			<head>
 				<HeadContent />
 			</head>
@@ -78,9 +78,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 							TanStackQueryDevtools,
 						]}
 					/>
+					<Toaster />
 				</ClerkProvider>
 				<Scripts />
-				<Toaster />
 			</body>
 		</html>
 	);
